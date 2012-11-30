989e9a131d36e3d6ee5662fdb42942ef0c598bf7
Andrew Sackville-West <andrew@swclan.homelinux.org>
2012-11-29 21:36:38 -0800

    add proper options handling
    includes unsupported input file option... coming soon ;)

--- hLife.hs
+++ hLife.hs
@@ -1,21 +1,51 @@
 import Life (randomBoard)
-import Life.Console as C (play, showBoard)
+import Life.Console as C (play)
 import Life.Gloss as G (play)
 
 import System.Random
 import System.Environment
+import System.Console.GetOpt
 
-defaultSize = 50 :: Int
+data Options = Options { useGloss  :: Bool
+                       , width     :: Int
+                       , height    :: Int
+                       , inputFile :: Maybe FilePath
+                       }
+
+defaultOptions :: Options
+defaultOptions =  Options { useGloss  = False
+                          , width     = 50
+                          , height    = 50
+                          , inputFile = Nothing
+                          }
 
 main :: IO ()
 main = do
        args <- getArgs
-       let (width, height) = case args of
-             a@[_, _]       | [w', h'] <- map read a -> (w', h')
-             a@["-g", _, _] | [w', h'] <- map read (tail a) -> (w', h')
-             _ -> (defaultSize, defaultSize)
-       seed <- randomIO
-       let b = randomBoard seed width height
-       case args of
-         ("-g" : _) -> G.play b
-         _          -> C.play b
+       let (parsedOpts, _, msgs) = getOpt Permute options args
+           opts = foldl (\o f -> f o) defaultOptions parsedOpts
+       case msgs of
+         [] -> do
+           seed <- randomIO
+           let b = randomBoard seed (width opts) (height opts)
+           if useGloss opts
+             then G.play b
+             else C.play b
+         _ -> error $ concat msgs ++ usageInfo "Usage: hLife [OPTION...]" options
+
+options :: [ OptDescr (Options -> Options) ]
+options =  [ Option ['s'] ["size"]  (ReqArg readSize "SIZE") "board size as <width>x<height>"
+           , Option ['g'] ["gloss"] (NoArg setGloss)         "use the Gloss graphical interface"
+           , Option ['f'] ["file"]  (OptArg setFile "FILE")  "read the supplied input file (will center in SIZE)"
+           ]
+
+setGloss :: Options -> Options
+setGloss opts = opts { useGloss = True }
+
+readSize :: String -> Options -> Options
+readSize size opts = opts { width = w, height = h }
+  where w = read $ takeWhile (/='x') size
+        h = read . tail $ dropWhile (/= 'x') size
+
+setFile :: Maybe FilePath -> Options -> Options
+setFile file opts = opts { inputFile = file }
